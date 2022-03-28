d24d2e4e4310ae43bd2ae31f4fda09273628f755
IndraP24 <indrashis985@gmail.com>
2022-03-28 22:32:32 +0530

    Update config files and test_image filenames

--- vortex/notebooks/utils/config.py
+++ vortex/notebooks/utils/config.py
@@ -2,7 +2,7 @@
 import os
 # define the base path to the input dataset and then use it to derive
 # the path to the images directory and annotation CSV file
-BASE_PATH = "../../data/processed/Obj_Det/"
+BASE_PATH = "../../data/processed/Obj_Det"
 IMAGES_PATH = os.path.sep.join([BASE_PATH, "images"])
 ANNOTS_PATH = os.path.sep.join([BASE_PATH, "labels.csv"])
 
